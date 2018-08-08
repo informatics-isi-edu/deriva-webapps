bc62cb1cd995a45e147d020fadbe049a183f80e1
dhanashri <dhana.tidke@gmail.com>
2018-08-08 11:15:23 -0700

    Added icons to treeview nodes for a specific Specimen RID. Used a jstreegrid to modify the template of the tree. Parser modified to contain the data of the query that fetches extra attributes.

---
+++ treeview/jstree/global.js
@@ -0,0 +1,87 @@
+
+
+window.onload = function() {
+    start();
+}
+function search(){
+    var v=  $('#plugins4_q').val();
+    $('#using_json').jstree(true).search(v);
+    // $("#using_json").jstree('search');
+}
+function reset(){
+    document.getElementById('plugins4_q').value="";
+     $("#using_json").jstree('clear_search');
+     $("#using_json").jstree('close_all');
+}
+
+function expandAll(){
+    
+    $('.loader')[0].style.display="block";
+    $('#mainDiv')[0].style.display="none";
+    
+    //$("#using_json").jstree('clear_search');
+    $("#using_json").jstree(true).open_all();    
+    // var m = $('#using_json').jstree(true)._model.data, i;
+    // 
+    //     for(i in m) {
+    //         if(m.hasOwnProperty(i) && i !== '#') {
+    //             m[i].state.opened = true;
+    //         }
+    //     }
+    //     $('#using_json').jstree(true).redraw(true);
+    $('.loader')[0].style.display="none";
+    $('#mainDiv')[0].style.display="block";
+}
+
+function closeAll(){
+    $("#using_json").jstree('close_all');    
+}
+
+
+function buildTree(){
+    
+    console.log();
+    $.getJSON("global_parse.json", function(presentationData) {
+    $('#using_json').jstree({
+        'core' : {
+            'data' : presentationData
+        },
+        "types" : {
+           
+           "folder" : {
+               
+             "icon" : "glyphicon glyphicon-tree-deciduous",
+             "valid_children" : ["default"]
+           },
+           
+           "file" : {
+             "icon" : "glyphicon glyphicon-leaf",
+             "valid_children" : []
+           }
+       },
+       "plugins" : [
+            "search","sort","types"
+        ],
+         "search": { "show_only_matches" : false, "show_only_matches_children" : false, "close_opened_onclear": true}
+        });
+    });
+    document.getElementsByClassName('loader')[0].style.display="none";
+      document.getElementById('plugins4_q').style.visibility="visible";
+}
+    $(function () {
+      $("#plugins4").jstree({
+        "plugins" : [ "search" ]
+      });
+      var to = false;
+      $('#plugins4_q').keyup(function () {
+        if(to) { clearTimeout(to); }
+        to = setTimeout(function () {
+          var v = $('#plugins4_q').val();
+          $('#using_json').jstree(true).search(v);
+      }, 1400);
+      });
+    });
+
+function start(){
+    buildTree();
+}
---
+++ treeview/jstree/parserglobal.js
@@ -0,0 +1,222 @@
+var fs = require('fs');
+
+function init() {
+    console.log('**Parser Started**');
+    start();
+}
+var data;
+var jsonData;
+var forest={};
+function removeParent(presentationData){
+    
+    if(presentationData.children.length ==0){
+        presentationData['type'] = 'file';
+    }
+    for(var c=0;c<presentationData.children.length;c++){
+        presentationData['type'] = 'folder';
+        removeParent(presentationData.children[c]);
+    }
+    delete presentationData.parent;
+}
+function buildTree(presentationData){
+    for(var z=0; z< presentationData.length; z++){
+        removeParent(presentationData[z]);
+    }
+    return presentationData;
+}
+ function start(){
+      var presentationData =[];
+      //console.log(1+3);
+      var json = JSON.parse(fs.readFileSync('global_data.json', 'utf8'));
+      //console.log(json);
+      forest= processData(json);
+      var presentationData =[];
+      
+      for(var g=0; g< forest.trees.length; g++)
+          presentationData.push(forest.trees[g].node);
+        
+        var finalData = buildTree(presentationData);
+          const content = JSON.stringify(finalData);
+          //console.log(content);
+      fs.writeFile("global_parse.json", content, 'utf8', function (err) {
+          if (err) {
+              return console.log(err);
+          }
+
+          console.log("The file was saved!");
+      }); 
+          console.log("**END**");
+}
+
+function processData(data){
+
+    var parent =  {
+        text :  data[0].object +' ('+data[0].object_dbxref+')',
+        parent : [],
+        children : [],
+        dbxref : data[0].object_dbxref,
+        a_attr      : 
+            {
+                'href': '/chaise/record/#2/data_commons:cvterm/dbxref='+data[0].object_dbxref.replace(/:/g,'%3A'),
+                'style': 'display:inline;'
+            } 
+    };
+    var child =  {
+        text :  data[0].subject +' ('+data[0].subject_dbxref+')',
+        parent : [],
+        children : [],
+        dbxref : data[0].subject_dbxref,
+        a_attr      : 
+            {
+                'href': '/chaise/record/#2/data_commons:cvterm/dbxref='+data[0].subject_dbxref.replace(/:/g,'%3A'),
+                'style': 'display:inline;' 
+            }
+    };
+    var tree = new Tree(parent);
+    var tree1 = new Tree(child);
+    parent.children.push(child);
+    child.parent.push(parent);
+    //var tree = new Tree(parent);
+    var forest = new Forest(parent);
+    forest.trees.push(tree);
+    
+    
+    for(var i =1 ;i <data.length; i ++){
+        // if(data[i].object_dbxref == "UBERON:0010536:" || data[i].subject_dbxref == "UBERON:0010536:")
+        //     console.log('found');
+        var parent =  {
+            text :  data[i].object +' ('+data[i].object_dbxref+')',
+            parent : [],
+            children : [],
+            dbxref : data[i].object_dbxref,
+            a_attr      : {'href': '/chaise/record/#2/data_commons:cvterm/dbxref='+data[i].object_dbxref.replace(/:/g,'%3A')} 
+        };
+        var child =  {
+            text :  data[i].subject  +' ('+data[i].subject_dbxref+')',
+            parent : [],
+            children : [],
+            dbxref : data[i].subject_dbxref,
+            a_attr      : {'href': '/chaise/record/#2/data_commons:cvterm/dbxref='+data[i].subject_dbxref.replace(/:/g,'%3A')} 
+        };
+        var tree = new Tree(parent);
+        var tree1 = new Tree(child);
+        parent.children.push(child);
+        child.parent.push(parent);
+        var added = false;
+        var parentNode =false;
+        var childNode = false;
+        var childIndex =-1;
+        var parentIndex =-1;
+        for(var f=0; f<forest.trees.length && !added;f++){
+            var tree= forest.trees[f];
+            
+            if(!parentNode){
+                 parentNode = tree.contains(tree, data[i].object_dbxref);
+                 if(parentNode)
+                    parentIndex= f;
+            }
+            if(!childNode){
+                 childNode = tree.contains(tree, data[i].subject_dbxref);
+                 if(childNode)
+                    childIndex= f;
+            }
+        }
+        //parent and child both not found
+        // add this as a new tree and add to forest
+        if(!parentNode && !childNode){
+            var tree = new Tree(parent);
+            forest.trees.push(tree);
+        }
+        //parent node exist, add child to parent node
+        else if(parentNode && !childNode){
+            parentNode.children.push(child);
+        }
+        //child node exist, add parent to child node
+        //delete child from the forest as child is no longer root
+        else if(!parentNode && childNode){
+            parent.children.pop();
+            parent.children.push(childNode);
+            childNode.parent.push(parent);
+            tree = new Tree(parent);
+            jloop:
+            for(var t=0; t< forest.trees.length; t++){
+                if(forest.trees[t].node.dbxref == childNode.dbxref){
+                    //console.log(forest.trees[t].node.text);
+                    forest.trees.splice(t,1);
+                    break jloop;
+                }
+            }
+            forest.trees.push(tree);
+        }
+        //child and parent node, both are present then add child to parent
+        //and remove the child form the forest
+        else if(parentNode && childNode){
+            parentNode.children.push(childNode);
+            ploop:
+            for(var q=0; q< forest.trees.length; q++){
+                if(forest.trees[q].node.dbxref == childNode.dbxref){
+                    //console.log(forest.trees[q].node.text);
+                    forest.trees.splice(q,1);
+                    break ploop;
+                }
+            }
+        }
+    }
+    
+    return (forest);
+
+}
+function Queue(){var a=[],b=0;this.getLength=function(){return a.length-b};this.isEmpty=function(){return 0==a.length};this.enqueue=function(b){a.push(b)};this.dequeue=function(){if(0!=a.length){var c=a[b];2*++b>=a.length&&(a=a.slice(b),b=0);return c}};this.peek=function(){return 0<a.length?a[b]:void 0}};
+function Tree(node) {
+    var linkId= node.dbxref.replace(/:/g,'%3A');
+    var s= node.a_attr;
+    var l = "'/chaise/record/#2/data_commons:cvterm/dbxref="+linkId+"','_blank'";
+    s["onClick"]="window.open("+l+");";
+    var node = { text: node.text, dbxref: node.dbxref, children :node.children,
+         parent: node.parent,a_attr : s};
+    this.node = node;
+}
+var tress=[];
+function Forest(node) {
+    var tree = new Tree(node);
+    this.trees = [];
+} 
+Tree.prototype.traverseBF = function(dbxref) {
+    var queue = new Queue();
+    queue.enqueue(this.node);
+    currentNode = queue.dequeue();
+ 
+    while(currentNode){
+        for (var i = 0, length = currentNode.children.length; i < length; i++) {
+            queue.enqueue(currentNode.children[i]);
+        }
+        if(currentNode.dbxref == dbxref)
+            return currentNode;
+        currentNode = queue.dequeue();
+    }
+};
+
+
+ 
+Tree.prototype.contains = function(tree, dbxref) {
+    return tree.traverseBF(dbxref);
+};
+ 
+Tree.prototype.add = function(data, toData, traversal) {
+    var child =  { text: data, 
+            children:[],
+            parent : [],
+            callback : function(node) {
+                if (node.text === toData.text) {
+                    parent = node;
+                }
+            }};
+    this.contains(child.callback, traversal);
+    if (parent) {
+        parent.children.push(child);
+        child.parent = parent;
+    } else {
+        throw new Error('Cannot add node to a non-existent parent.');
+    }
+};
+init();
