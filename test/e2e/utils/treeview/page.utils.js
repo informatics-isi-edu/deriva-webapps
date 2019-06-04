ff32c0595024e8ecbd402d5fd71ffc385a4ce351
vipul <vipul21sept@gmail.com>
2019-06-04 09:59:54 -0700

    Folder structure for treeview

---
+++ test/e2e/utils/treeview/page.utils.js
@@ -0,0 +1,9 @@
+function treeviewPages() {
+    // individual pages
+    this.url = {
+        'dynamic' : '?Specimen_RID=N-GXNY',
+        'record'  : '/chaise/record/#2/Gene_Expression:Specimen/RID=N-GXNY'
+    }
+}
+
+module.exports = new treeviewPages();
