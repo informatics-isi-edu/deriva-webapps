c967b6823354b7765d38f7f0b08c60ff507484f3
Vipul Singh <vipul21sept@gmail.com>
2019-08-05 11:01:05 -0700

    Readme and cleanup of code for plotapp

--- plot/plot-config.js
+++ plot/plot-config.js
@@ -1,32 +1,3 @@
-# plot-app
-## Note
-This app uses Chaise assuming Chaise is at the same level as deriva-webapps folder.
-
-## Configuration
-The plot-config.js file has the following parameters which can be modified when using this app.
-
-### Data parameters
-1. title: Title of the plot app.
-2. plots: An array of the plots to be shown.
-
-Each plot will have the following parameters:
-
-1. plot_title: Title to be displayed above the line plot.
-2. x_axis_label: Label to be displayed along the x-axis.
-3. y_axis_label: Label to be displayed along the y-axis.
-4. plot_type: The type of plot to be displayed i.e `line`, `bar`, `dot`, `area`, `dot+lines` .
-5. traces: Contains the information about each each trace.
-      1. uri : The Url from which the data has to be fetched.
-      2. legend: The value of legend to be shown for this trace.
-      3. x_col: The column name for the x values
-      4. y_col: An array of column name for the y values
-6. PlotlyButtonsToRemove: The button to be removed shown by plotly by defualt.
-
-#### Note
-If any of the above (Presentation or data) values is not mentioned, the app will throw an error. If you don't want to set a value, set it to `null`.
-
-### Sample lineplot-config.js (also included in the repo)
-```javascript
 var plotConfig = {
     title: "2dPlot",                                                            // Title of the page
     plots: [{                                                                   // Array of object plots to be shown on the page
@@ -57,9 +28,3 @@ var plotConfig = {
 if (typeof module === 'object' && module.exports && typeof require === 'function') {
     exports.config = plotConfig;
 }
-```
-
-## Installation
-
-### Installation Path
-Change the installation path by changing the value of INSDIR in Makefile and then execute the "make install" command.
---
+++ plot/plot.app.js
@@ -0,0 +1,263 @@
+(function () {
+    'use strict';
+
+    angular.module('chaise.configure-plotApp', ['chaise.config'])
+
+        .constant('appName', 'plotApp')
+
+        .run(['$rootScope', function ($rootScope) {
+            // When the configuration module's run block emits the `configuration-done` event, attach the app to the DOM
+            $rootScope.$on("configuration-done", function () {
+                angular.element(document).ready(function () {
+                    angular.bootstrap(document.getElementById("plot"), ["plotApp"]);
+                });
+            });
+        }]);
+
+        angular.module('plotApp', [
+            'ngSanitize',
+            'ngCookies',
+            'chaise.alerts',
+            'chaise.filters',
+            'chaise.inputs',
+            'chaise.utils',
+            'ermrestjs',
+            'ui.bootstrap',
+            'chaise.errors',
+            'chaise.navbar'
+          ])
+
+            .config(['$cookiesProvider', function ($cookiesProvider) {
+                $cookiesProvider.defaults.path = '/';
+            }])
+            .factory('PlotUtils', ['AlertsService', 'ConfigUtils', 'dataFormats', 'Session', 'UriUtils', '$rootScope', function (AlertsService, ConfigUtils, dataFormats, Session, UriUtils, $rootScope) {
+                var ermrestServiceUrl = ConfigUtils.getConfigJSON().ermrestLocation;
+                var contextHeaderParams = {"cid": "2d-plot"};
+                var server = ERMrest.ermrestFactory.getServer(ermrestServiceUrl, contextHeaderParams);
+
+                function getType(type) {
+                  switch (type) {
+                    case "line":
+                      return "scatter";
+                    case "bar":
+                      return "bar";
+                    case "dot":
+                      return "scatter";
+                    case "dot-lines":
+                      return "scatter";
+                    case "area":
+                      return "area";
+                    default:
+                      return "scatter";
+                  }
+                }
+
+                function getMode(type) {
+                  switch (type) {
+                    case "line":
+                      return "lines";
+                    case "bar":
+                      return "";
+                    case "dot":
+                      return "markers";
+                    case "dot-lines":
+                      return "lines+markers";
+                    case "area":
+                      return "";
+                    default:
+                      return "line+markers";
+                  }
+                }
+
+                return {
+                    getData: function () {
+                        var plots = {
+                          data: []
+                        };
+                        var tracesComplete = 0;
+                        var first_plot = plotConfig.plots[0]; // TODO: Currently picking  only the first plot config, make it more multiple plots
+                        first_plot.traces.forEach(function (trace) {
+                            server.http.get(trace.uri).then(function(response) {
+
+                                var layout = {
+                                    title: first_plot.plot_title,
+                                    xaxis: {
+                                        title: first_plot.x_axis_label
+                                    },
+                                    yaxis: {
+                                        title: first_plot.y_axis_label
+                                    },
+                                    responsive: true,
+                                    autosize: true,
+                                    width: 1000,
+                                    height: 500,
+                                    showlegend: true,
+                                };
+                                var config = {
+                                  displaylogo: false,
+                                  modeBarButtonsToRemove: first_plot.plotlyDefaultButtonsToRemove
+                                };
+                                var data = response.data;
+
+                                for(var i = 0; i < trace.y_col.length;i++) {
+                                  var values = {
+                                      x: [],
+                                      y: [],
+                                      type: getType(first_plot.plot_type),
+                                      name: trace.legend[i] || '',
+                                      fill: first_plot.plot_type == 'area' ? 'tozeroy':'',
+                                      mode: getMode(first_plot.plot_type)
+                                  }
+                                  data.forEach(function (row) {
+                                      values.x.push(row[trace.x_col]);
+                                      values.y.push(row[trace.y_col[i]]);
+                                  });
+
+                                  plots.data.push(values);
+                                  plots.layout = layout;
+                                  plots.config = config;
+                                }
+
+                                tracesComplete++;
+                                if (tracesComplete == first_plot.traces.length) {
+                                  $rootScope.plots = plots;
+                                  Plotly.relayout();
+                                }
+                            }).catch(function (err) {
+                                if (err.data) {
+                                    if (err.status == 401) {
+                                        if (!$rootScope.loginShown) {
+                                            $rootScope.loginShown = true;
+                                            Session.loginInAModal(function () {
+                                                // set to false in case a request fails after with 401
+                                                $rootScope.loginShown = false;
+                                                window.location.reload();
+                                            });
+                                        }
+                                    } else {
+                                        // else add warning alert
+                                        AlertsService.addAlert(err.data, 'warning');
+                                        throw ERMrest.responseToError(err);
+                                    }
+                                }
+                            });
+                        });
+                    }
+                }
+            }])
+            .controller('plotController', ['AlertsService', 'dataFormats', 'PlotUtils', 'UriUtils', '$window', '$rootScope', '$scope', '$timeout', function plotController(AlertsService, dataFormats, PlotUtils, UriUtils, $window, $rootScope, $scope, $timeout) {
+                var vm = this;
+                vm.alerts = AlertsService.alerts;
+                vm.dataFormats = dataFormats;
+                vm.x_label = plotConfig.x_axis_label;
+                vm.types = ["line", "bar", "dot", "area"];
+                vm.model = {
+                  title: plotConfig.title,
+                  type : {
+                    name: plotConfig.plot_type,
+                  }
+                }
+                vm.changeSelection = function(value) {
+                  var plotsData = $rootScope.plots.data;
+                  var layout = $rootScope.plots.layout;
+                  var config = $rootScope.plots.config;
+                  switch (vm.model.type.name) {
+                    case 'bar':
+                      var newPlotData = [];
+                      plotsData.forEach(function (row) {
+                          row.type="bar";
+                          newPlotData.push(row);
+                      });
+                      $rootScope.plots = {
+                        data: newPlotData,
+                        layout: layout,
+                        config: config
+                      };
+                      break;
+                    case 'line':
+                      var newPlotData = [];
+                      plotsData.forEach(function (row) {
+                          row.type="scatter";
+                          row.mode="lines+markers";
+                          row.fill = "";
+                          newPlotData.push(row);
+                      });
+                      $rootScope.plots = {
+                        data: newPlotData,
+                        layout: layout,
+                        config: config
+                      };
+                      break;
+                    case "dot":
+                      var newPlotData = [];
+                      plotsData.forEach(function (row) {
+                          row.type="scatter";
+                          row.mode="markers"
+                          newPlotData.push(row);
+                      });
+                      $rootScope.plots = {
+                        data: newPlotData,
+                        layout: layout,
+                        config: config
+                      };
+                      break;
+                    case "area":
+                      var newPlotData = [];
+                      plotsData.forEach(function (row) {
+                          row.type="scatter";
+                          row.fill = "tozeroy";
+                          newPlotData.push(row);
+                      });
+                      $rootScope.plots = {
+                        data: newPlotData,
+                        layout: layout,
+                        config: config,
+                      };
+                      break;
+                    default:
+                      break;
+                  }
+                }
+
+                vm.plotsLoaded = false;
+                $scope.showPlot = function () {
+                	if (vm.model.user == null) {
+                		vm.model.user = $rootScope.user;
+                	}
+                    $scope.$apply(function () {
+                        vm.plotsLoaded = true;
+                    });
+                }
+            }])
+            .directive('plot', ['$rootScope', function ($rootScope) {
+                return {
+                    link: function (scope, element) {
+                        scope.$watch('plots', function (plots) {
+                            if (plots) {
+                                Plotly.newPlot(element[0], plots.data, plots.layout, plots.config).then(function () {
+                                    scope.showPlot();
+                                }.bind(1));
+                            }
+                        }, true);
+                    }
+                };
+            }])
+            .run(['ERMrest', 'PlotUtils', 'messageMap', 'Session', '$rootScope',
+             function runApp(ERMrest, PlotUtils, messageMap, Session, $rootScope) {
+               try {
+                 $rootScope.loginShown = false;
+                 var subId = Session.subscribeOnChange(function () {
+                   Session.unsubscribeOnChange(subId);
+                   var session = Session.getSessionValue();
+                   if (!session) {
+                       var notAuthorizedError = new ERMrest.UnauthorizedError(messageMap.unauthorizedErrorCode, (messageMap.unauthorizedMessage + messageMap.reportErrorToAdmin));
+                       throw notAuthorizedError;
+                   }
+                   PlotUtils.getData();
+                 });
+               } catch (exception) {
+                   throw exception;
+               }
+             }
+         ]);
+})();
