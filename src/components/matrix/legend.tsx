30391c5be39817555b3e9d7566a106d7e87c1658
cheekw <kweic8@gmail.com>
2023-02-01 12:35:30 -0800

    add legend

---
+++ src/components/matrix/legend.tsx
@@ -0,0 +1,30 @@
+import '@isrd-isi-edu/deriva-webapps/src/assets/scss/_matrix.scss';
+
+import Cell, { CellProps } from '@isrd-isi-edu/deriva-webapps/src/components/matrix/cell';
+import HeaderRow, {
+  HeaderRowProps,
+} from '@isrd-isi-edu/deriva-webapps/src/components/matrix/header-row';
+
+export type LegendProps = {
+  cells: Array<CellProps>;
+  header?: HeaderRowProps;
+};
+
+const Legend = ({ cells, header }: LegendProps): JSX.Element => {
+  const LegendRow = (): JSX.Element => (
+    <div className='row legend-row'>
+      {cells.length > 0
+        ? cells.map((data) => <Cell key={data.id} className='legend-cell' {...data} />)
+        : null}
+    </div>
+  );
+
+  return (
+    <div className='legend'>
+      <LegendRow />
+      <HeaderRow {...header} />
+    </div>
+  );
+};
+
+export default Legend;
