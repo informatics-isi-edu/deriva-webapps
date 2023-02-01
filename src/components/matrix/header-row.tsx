db51c5bdba5677887b0c3185076cbd7837492f79
cheekw <kweic8@gmail.com>
2023-02-01 12:34:45 -0800

    matrix table header

---
+++ src/components/matrix/header-row.tsx
@@ -0,0 +1,35 @@
+import '@isrd-isi-edu/deriva-webapps/src/assets/scss/_matrix.scss';
+
+export type HeaderRowProps = {
+  className?: string;
+  headers?: Array<{
+    id?: string;
+    className?: string;
+    link?: string;
+    title?: string;
+  }>;
+};
+
+const HeaderRow = ({ className, headers }: HeaderRowProps): JSX.Element => {
+  const RowElements = (): JSX.Element => (
+    <>
+      {headers?.map(({ id, className, title, link }) => {
+        return title ? (
+          <div key={id} className={`${className ? className : 'matrix-text-container'}`}>
+            <a className={'link-text'} href={link} title={title}>
+              {title}
+            </a>
+          </div>
+        ) : null;
+      })}
+    </>
+  );
+
+  return (
+    <div className={`row ${className ? className : ''}`}>
+      {headers && headers.length > 0 ? <RowElements /> : null}
+    </div>
+  );
+};
+
+export default HeaderRow;
