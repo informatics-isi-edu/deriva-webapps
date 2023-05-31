42870047cdff117145d299b0fbca0b1b35efef4f
KenyShah24 <122313521+KenyShah24@users.noreply.github.com>
2023-05-31 13:16:17 -0700

    React heatmap (#147)
    * skeletong for react heatmap app
    * Heatmap module migration to React
    * Added js docs comments for all functions and changes in code as per dev guidelines
    * Created common styles file for heatmap
    
    ---------
    
    Co-authored-by: Joshua Chudy <jchudy@isi.edu>

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -3,4 +3,5 @@ declare module '*.png';
 declare module '*.jpeg';
 declare module '*.gif';
 declare module 'plotly.js-basic-dist-min';
+declare module 'plotly.js-cartesian-dist-min';
 declare module 'react-plotly.js/factory';
