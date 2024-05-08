b63cb51ebe83be3eb9f8b0958e4fc11f43534423
Joshua Chudy <jchudy@isi.edu>
2024-05-08 12:53:25 -0700

    use DisplayValue component from chaise instead of using parts of that implementation in label.tsx

--- src/components/controls/label.tsx
+++ src/components/controls/label.tsx
@@ -1,5 +1,5 @@
-// hooks
-import { useEffect, useRef } from 'react';
+// components
+import DisplayValue from '@isrd-isi-edu/chaise/src/components/display-value';
 
 // models
 import { UserControlConfig } from '@isrd-isi-edu/deriva-webapps/src/models/webapps-core';
@@ -7,37 +7,31 @@ import { PlotTemplateParams } from '@isrd-isi-edu/deriva-webapps/src/models/plot
 import { VitessceTemplateParams } from '@isrd-isi-edu/deriva-webapps/src/models/vitessce';
 
 // utils
-import { createChaiseTooltips } from '@isrd-isi-edu/chaise/src/utils/ui-utils';
 import { convertKeysSnakeToCamel, createLink } from '@isrd-isi-edu/deriva-webapps/src/utils/string';
 
 /**
  * MarkdownProps is the type of props for Markdown component.
  */
 export type LabelProps = {
-    userControlConfig: UserControlConfig;
-    templateParams: PlotTemplateParams | VitessceTemplateParams
+  userControlConfig: UserControlConfig;
+  templateParams: PlotTemplateParams | VitessceTemplateParams
 };
 
 /**
  * Label is a component that renders a link/text (<a> input).
  */
 const Label = ({
-    userControlConfig,
-    templateParams
+  userControlConfig,
+  templateParams
 }: LabelProps): JSX.Element => {
-    
-    const link = createLink(userControlConfig.label.markdown_pattern, templateParams);
-    const labelStyles = convertKeysSnakeToCamel(userControlConfig.label.styles); 
-    // handle tooltips that might be in the value
-    const spanRef = useRef<HTMLLabelElement | null>(null);
-    useEffect(() => {
-        if (!spanRef.current) return;
-        createChaiseTooltips(spanRef.current);
-    }, []);
-
-    return link ?
-        <label ref={spanRef} className="control-label" style={labelStyles} dangerouslySetInnerHTML={{ __html: link }} />
-        : <></>;
+  return (
+    <DisplayValue
+      value={{ isHTML: true, value: createLink(userControlConfig.label.markdown_pattern, templateParams) }}
+      addClass
+      className='control-label'
+      styles={convertKeysSnakeToCamel(userControlConfig.label.styles)}
+    />
+  );
 }
 
 export default Label;
